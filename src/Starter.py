dd8914ec008c24c0759be0bb2aa30e708a2e3f31
raztheskid <134842835+raztheskid@users.noreply.github.com>
2025-02-21 15:38:09 -0500

    Update Starter.py

--- src/Starter.py
+++ src/Starter.py
@@ -2,9 +2,9 @@ from time import sleep
 from rich.console import Console
 
 console = Console()
-tasks = [f"Successfully launched! " for n in range(1, 2)]
+tasks = [f"Successfully launched! " for _ in range(1, 2)]  # Fixed the list comprehension
 
-with console.status("[bold green]Launch DDos please wait a few seconds ...[/bold green]") as status:
+with console.status("[bold green]Launching DDos, please wait a few seconds...[/bold green]") as status:
     while tasks:
         task = tasks.pop(0)
         sleep(5)
